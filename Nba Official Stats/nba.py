14e02046e68b22510eccedff5237590edaac72a5
jayptz <jaypt2105@gmail.com>
2025-03-26 16:04:20 -0400

    player

--- Nba Official Stats/nba.py
+++ Nba Official Stats/nba.py
@@ -1,4 +1,5 @@
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
 
 # Nikola Jokić
 career = playercareerstats.PlayerCareerStats(player_id='203999') 
@@ -11,3 +12,17 @@ career.get_json()
 
 # dictionary
 career.get_dict()
+
+def get_active_players():
+    active_players = [player for player in players.get_players() if player['is_active']]
+    return active_players
+
+active_players = get_active_players()
+print(active_players[:5])
+
+
+
+
+
+
+
